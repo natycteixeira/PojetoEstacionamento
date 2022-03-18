b7977366f7eadef4613e18c13a2c247400c05324
natycteixeira <101603662+natycteixeira@users.noreply.github.com>
2022-03-17 22:13:42 -0300

    Add files via upload

---
+++ Aula2/src/view/LoginView.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.util.Scanner;
+import model.Usuario;
+
+public class LoginView {
+
+	public void login () {
+		Scanner leia = new Scanner (System.in);
+				
+		Usuario usuario = new Usuario();
+
+		System.out.println("***LOGIN***\n-\n-");
+		System.out.println("Informe o usuáio:");
+		usuario.setLogin(leia.next());
+		System.out.println("Informe a senha:");
+		usuario.setSenha(leia.next());
+		System.out.println("-\n-Logado com sucesso!");
+		System.out.println("***    ***");
+		
+	}
+}
